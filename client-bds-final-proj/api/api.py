1432b9207bc75a88dc710b16fcd5f985f25348f8
Josh Loesch <jloesch30@gmail.com>
2021-12-04 14:33:03 -0600

    working on bds final proj

--- client-bds-final-proj/api/api.py
+++ client-bds-final-proj/api/api.py
@@ -3,6 +3,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# serve all of my data stuff
+# I will train the model offline and export it via joblib
+
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
